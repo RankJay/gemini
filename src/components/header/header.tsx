809cd3e1501ef35107a18c8e78b8bb1f41053e6a
RankJay <rankjay0101@gmail.com>
2024-04-12 21:51:26 +0530

    adding gmail auth

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,10 @@
-import { Share } from "lucide-react";
-import { Button } from "../ui/button";
+import Profile from "./profile";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
+    <header className="sticky top-0 z-10 flex h-[53px] items-center justify-between gap-1 border-b bg-background px-4">
       <h1 className="text-xl tracking-tight font-bold">Email AI</h1>
-      <Button variant="outline" size="sm" className="ml-auto gap-1.5 text-sm">
-        <Share className="size-3.5" />
-        Share
-      </Button>
+      <Profile />
     </header>
   );
 }
---
+++ src/components/header/profile.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Share } from "lucide-react";
+import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { useEffect, useState } from "react";
+import { getCookie, deleteCookie } from "cookies-next";
+import { Skeleton } from "../ui/skeleton";
+import { useRouter } from "next/navigation";
+
+export default function Profile() {
+  const router = useRouter();
+  const [profileImage, setProfileImage] = useState<string>("");
+  const [ isExistingUser, setIsExistingUser ] = useState<boolean>(false);
+
+  useEffect(() => {
+    const token = getCookie("auth");
+    if (token) {
+      setIsExistingUser(true);
+    }
+    if (token) {
+      fetch("https://www.googleapis.com/oauth2/v1/userinfo", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Failed to fetch user info!");
+        })
+        .then((data) => {
+          setProfileImage(data.picture);
+        })
+        .catch((error) => {
+          deleteCookie("auth");
+        });
+    }
+  }, []);
+
+  if (isExistingUser || profileImage.length > 0) {
+    return (
+      <div>
+        <Avatar className="w-[24px] h-[24px]">
+          <AvatarImage src={profileImage} />
+          <AvatarFallback>
+            <Skeleton className="w-[24px] rounded-full" />
+          </AvatarFallback>
+        </Avatar>
+      </div>
+    );
+  }
+
+  return (
+    <Button variant="outline" size="sm" className="ml-auto gap-1.5 text-sm" onClick={() => router.push('/api/auth')}>
+      Sign in
+    </Button>
+  );
+}
