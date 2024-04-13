1aa20a3e08e315ea0dd46256cd8f707d907f6167
luckyrp2 <147841151+luckyrp2@users.noreply.github.com>
2024-04-13 13:04:06 -0400

    done

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,7 +1,9 @@
 import { useEmail } from "@/hooks/use-email-mode";
-import GSuiteSettings from "./gsuite/settings";
-import EmailMode from "./mode/mode";
 import ModelSettings from "./model/settings";
+import EmailMode from "./mode/mode";
+import GSuiteSettings from "./gsuite/settings";
+import EmailInput from "./gsuite/email/recipient";
+
 
 export type SettingsProps = {
   asDrawer?: boolean;
@@ -10,6 +12,7 @@ export type SettingsProps = {
 const SettingsForm = () => {
   return (
     <form className="grid w-full items-start gap-6">
+      <EmailInput />
       <EmailMode />
       <fieldset className="grid gap-6 rounded-lg border p-4">
         <legend className="-ml-1 px-1 text-sm font-medium">Settings</legend>
