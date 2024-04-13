9a1998fec3b46d657de3e20101f983120ff41f18
RankJay <rankjay0101@gmail.com>
2024-04-13 22:57:31 +0530

    adding email send endpoint

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -4,40 +4,90 @@ import { NextResponse } from "next/server";
 const MAX_RESULTS = process.env.MAX_RESULTS || 10;
 
 export async function GET(request: Request) {
-  const store = cookies();
-  const code = store.get("auth");
+  try {
+    const store = cookies();
+    const code = store.get("auth");
 
-  if (!code) {
-    return new Response("Unauthorized", { status: 401 });
-  }
-
-  const response = await fetch(
-    `https://gmail.googleapis.com/gmail/v1/users/me/messages?maxResults=${MAX_RESULTS}&labelIds=SENT`,
-    {
-      headers: {
-        Authorization: `Bearer ${code.value}`,
-      },
+    if (!code) {
+      return new Response("Unauthorized", { status: 401 });
     }
-  );
 
-  const messagesList = await response.json();
+    const response = await fetch(
+      `https://gmail.googleapis.com/gmail/v1/users/me/messages?maxResults=${MAX_RESULTS}&labelIds=SENT`,
+      {
+        headers: {
+          Authorization: `Bearer ${code.value}`,
+        },
+      }
+    );
 
-  console.log(messagesList);
+    const messagesList = await response.json();
 
-  const messages = await Promise.all(
-    messagesList.messages.map(async (message: any) => {
-      const response = await fetch(
-        `https://gmail.googleapis.com/gmail/v1/users/me/messages/${message.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${code.value}`,
-          },
-        }
-      );
+    const messages = await Promise.all(
+      messagesList.messages.map(async (message: any) => {
+        const response = await fetch(
+          `https://gmail.googleapis.com/gmail/v1/users/me/messages/${message.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${code.value}`,
+            },
+          }
+        );
 
-      return response.json();
-    })
-  );
+        return response.json();
+      })
+    );
 
-  return NextResponse.json(messages);
+    return NextResponse.json(messages);
+  } catch (error: any) {
+    console.log(error.response.data);
+    return new Response(error as any, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const store = cookies();
+    const code = store.get("auth");
+
+    if (!code) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await request.json();
+    const { to, subject, message } = body;
+
+    const email = [
+      'Content-Type: text/html; charset="UTF-8"\n',
+      "MIME-Version: 1.0\n",
+      `To: ${to}\n`,
+      `Subject: ${subject}\n\n`,
+      message,
+    ].join("");
+
+    const encodedEmail = Buffer.from(email)
+      .toString("base64")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
+
+    const response = await fetch(
+      `https://gmail.googleapis.com/gmail/v1/users/me/messages/send`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${code.value}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          raw: encodedEmail,
+        }),
+      }
+    );
+
+    return NextResponse.json({ data: await response.json() });
+  } catch (error: any) {
+    console.log(error.response.data);
+    return new Response(error as any, { status: 500 });
+  }
 }
