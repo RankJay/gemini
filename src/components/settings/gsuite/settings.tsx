6603a68bcebd798012434c7b45cf5a2c3f8b0a8b
RankJay <rankjay0101@gmail.com>
2024-04-17 23:02:35 +0530

    adding search/send in gsuite settings

--- src/components/settings/gsuite/settings.tsx
+++ src/components/settings/gsuite/settings.tsx
@@ -1,13 +1,42 @@
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { useGSuiteService } from "@/hooks/use-gsuite-service";
-import { GSuiteService } from "@/lib/types";
+import { EmailMode, GSuiteService } from "@/lib/types";
 import GSuiteDriveSettings from "./drive/drive";
 import GSuiteMeetSettings from "./meet/meet";
+import { Input } from "@/components/ui/input";
+import { useEmail } from "@/hooks/use-email-mode";
+import { Button } from "@/components/ui/button";
+
+function DraftEmailGSuiteSettings() {
+  return (
+    <>
+      <Input placeholder="Subject: You are using a great product!" />
+      <Input
+        placeholder="To: sundar.p@google.com"
+        pattern="^([a-zA-Z0-9. _%+-]+@[a-zA-Z0-9. -]+\\\\. [a-zA-Z]{2,}$/"
+      />
+      <Button>Send email</Button>
+    </>
+  );
+}
+
+function SearchEmailGSuiteSettings() {
+  return (
+    <>
+      <Input placeholder="Search for email..." />
+      <Button>Search</Button>
+    </>
+  );
+}
 
 export default function GSuiteSettings() {
   const { setGSuiteService } = useGSuiteService();
+  const { emailMode } = useEmail();
+
   return (
     <>
+      {emailMode === EmailMode.DRAFT && <DraftEmailGSuiteSettings />}
+      {emailMode === EmailMode.SEARCH && <SearchEmailGSuiteSettings />}
       <Tabs defaultValue={GSuiteService.DRIVE} className="w-[200px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger
